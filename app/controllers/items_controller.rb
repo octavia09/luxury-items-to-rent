6004793ae6fd642893ec609b01e2e25ef992480a
Octavia von Bernuth <octavia@bernuth.de>
2021-03-01 17:16:01 +0400

    Show page and Search button

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,15 @@
 class ItemsController < ApplicationController
 
-def index
-  @items = Item.all
-end
+  def index
+    raise
+    if params[:search].present?
+      @items = Item.where(category: params[:search][:query])
+    else
+      @items = Item.all
+    end
+  end
 
+  def show
+    @item = Item.find(params[:id])
+  end
 end
