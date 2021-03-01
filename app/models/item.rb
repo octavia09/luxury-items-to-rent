66fbe3c459caeb7caa4393113da86f9c284c0873
Octavia von Bernuth <octavia@bernuth.de>
2021-03-01 18:48:35 +0400

    added sizes to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
   validates_presence_of :name, :description, :size, :price, :category
+  SIZES = ["XS", "S", "M", "L", "XL", "XXL"]
   CATEGORIES = ["bag", "dress", "shoe"]
   validates :category, inclusion: { in: CATEGORIES }
   has_many_attached :images
